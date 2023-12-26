c2893907c92ca1fd43cea2b73cffb82244dab08b
Thanh3102 <thanhduong200602@gmail.com>
2023-12-26 15:03:57 +0700

    Tab

--- app/src/main/java/com/example/coffeeorderjavaapp/MainActivity.java
+++ app/src/main/java/com/example/coffeeorderjavaapp/MainActivity.java
@@ -1,9 +1,19 @@
 package com.example.coffeeorderjavaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.coffeeorderjavaapp.adapter.MainViewPagerAdapter;
+import com.example.coffeeorderjavaapp.adapter.ProductAdapter;
+import com.example.coffeeorderjavaapp.model.Product;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +22,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, ProductListActivity.class);
-        startActivity(intent);
+        TabLayout tabLayout = findViewById(R.id.navigationBottomLayout);
+        ViewPager2 viewPager = findViewById(R.id.mainViewPager);
+        MainViewPagerAdapter vpAdapter = new MainViewPagerAdapter(this);
+        viewPager.setAdapter(vpAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+                tabLayout.getTabAt(position).select();
+            }
+        });
+
+//        List<Product> products = new ArrayList<Product>();
+//        products.add(new Product(1, "Cà Phê Sữa Đá", "Cà phê", 29000));
+//        products.add(new Product(2, "Caramel Muối Phin Sữa Tươi", "Cà phê", 69000));
+//        products.add(new Product(3, "Frosty Caramel Muối Arabica", "Cà phê", 72000));
+//        products.add(new Product(4, "Trà Xanh Espresso Marble", "Trà xanh", 49000));
+//        products.add(new Product(5, "Cà Phê Sữa Nóng", "Cà phê", 39000));
+//        products.add(new Product(6, "CloudTea Trà Xanh Tây Bắc", "Cloud", 69000));
+//        products.add(new Product(7, "CloudTea Oolong Nướng Kem Dừa Đá Xay", "Cloud", 55000));
+//        products.add(new Product(8, "Frosty Phin-Gato", "Đá xay", 55000));
+//
+//        RecyclerView productRecView = this.findViewById(R.id.productListRecView);
+//        ProductAdapter productAdapter = new ProductAdapter(products);
+//        productRecView.setAdapter(productAdapter);
     }
 }
---
+++ app/src/main/java/com/example/coffeeorderjavaapp/adapter/MainViewPagerAdapter.java
@@ -0,0 +1,36 @@
+package com.example.coffeeorderjavaapp.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.coffeeorderjavaapp.fragment.ProductListFragment;
+
+public class MainViewPagerAdapter extends FragmentStateAdapter {
+    public MainViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 0:
+                return new ProductListFragment();
+            case 1:
+                return new ProductListFragment();
+            case 2:
+                return new ProductListFragment();
+            case 3:
+                return new ProductListFragment();
+            default:
+                return new ProductListFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 4;
+    }
+}

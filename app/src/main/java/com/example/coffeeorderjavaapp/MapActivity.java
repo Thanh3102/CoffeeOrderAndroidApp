53ffd9feb80d39b9d308167c82ee0335b02ad052
Thanh3102 <thanhduong200602@gmail.com>
2024-01-12 17:03:45 +0700

    fix google map location

--- app/src/main/java/com/example/coffeeorderjavaapp/MapActivity.java
+++ app/src/main/java/com/example/coffeeorderjavaapp/MapActivity.java
@@ -1,13 +1,10 @@
 package com.example.coffeeorderjavaapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,17 +12,19 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Locale;
 
 public class MapActivity extends AppCompatActivity implements
@@ -34,28 +33,34 @@ public class MapActivity extends AppCompatActivity implements
     private GoogleMap map;
     private final String[] permissionArray = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
     private final int requestCode = 1;
+    private Marker shippingMark;
     private Marker currentMark;
 
     private TextView markerTv;
 
-    private double currentLat;
-    private double currentLng;
+    private Location currentLocation;
+
+    private LocationManager mLocationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         this.markerTv = findViewById(R.id.markerLocationTv);
+        if(currentLocation == null){
+            markerTv.setText("Vị trí nhận hàng: Chưa chọn vị trí");
+        }
 
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MapActivity.this, permissionArray, requestCode);
         } else {
-            LocationManager mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
             mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000,
-                    500, location -> {
+                    100, location -> {
                         Toast.makeText(this, "Current location " + location, Toast.LENGTH_SHORT).show();
                         Log.i("Location", "Current location " + location);
+                        this.currentLocation = location;
                     });
         }
 
@@ -69,12 +74,20 @@ public class MapActivity extends AppCompatActivity implements
         Button testBtn = findViewById(R.id.test);
         Button backBtn = findViewById(R.id.mapBack);
         testBtn.setOnClickListener(v -> {
-            if (map == null) {
+            if (map == null || currentLocation == null) {
                 Toast.makeText(this, "Map is not ready", Toast.LENGTH_SHORT).show();
             } else {
-                LatLng currentPosition = new LatLng(currentLat, currentLng);
+                Toast.makeText(this, "Lat: " + currentLocation.getLatitude() + " Lng: " + currentLocation.getLongitude() , Toast.LENGTH_SHORT).show();
+                if (this.currentMark != null) {
+                    this.currentMark.remove();
+                }
+                this.currentMark = map.addMarker(new MarkerOptions()
+                        .position(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()))
+                        .title("Vị trí của bạn"));
+                LatLng currentPosition = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
                 map.moveCamera(CameraUpdateFactory.newLatLng(currentPosition));
-                map.animateCamera(CameraUpdateFactory.zoomTo(15));
+                map.animateCamera(CameraUpdateFactory.zoomTo(18));
+                Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -90,22 +103,20 @@ public class MapActivity extends AppCompatActivity implements
             Geocoder geocoder = new Geocoder(this, Locale.getDefault());
             try {
                 Address currentAddress = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1).get(0);
-                if (this.currentMark != null) {
-                    this.currentMark.remove();
+                if (this.shippingMark != null) {
+                    this.shippingMark.remove();
                 }
-                this.currentMark = map.addMarker(new MarkerOptions()
+                this.shippingMark = map.addMarker(new MarkerOptions()
                         .position(new LatLng(latLng.latitude, latLng.longitude))
-                        .title("Vị trí giao hàng"));
-                markerTv.setText(currentAddress.getAddressLine(0).toString());
+                        .title("Vị trí nhận hàng"));
+                markerTv.setText("Vị trí nhận hàng: " + currentAddress.getAddressLine(0).toString());
                 Log.i("Click location", currentAddress.toString());
                 Log.i("Address line", currentAddress.getAddressLine(0).toString());
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         });
-//        googleMap.addMarker(new MarkerOptions()
-//                .position(new LatLng(0, 0))
-//                .title("Marker"));
+
 
     }
 
@@ -116,19 +127,17 @@ public class MapActivity extends AppCompatActivity implements
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
         if (requestCode == this.requestCode) {
-            LocationManager mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
             if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
                 finish();
-                return;
+            } else {
+                mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000,
+                        100, location -> {
+                            Toast.makeText(this, "Current location " + location, Toast.LENGTH_SHORT).show();
+                            Log.i("Location", "Current location " + location);
+                            this.currentLocation = location;
+                        });
             }
-            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000,
-                    500, location -> {
-                        Toast.makeText(this, "Current location: " + location, Toast.LENGTH_SHORT).show();
-                        Log.i("Location", "Current location " + location);
-                        currentLat = location.getLatitude();
-                        currentLng = location.getLongitude();
-                    });
         }
     }
 }
